877c05f3ea2c47def65ac2c684346f6f1fbebcdb
CRW <chris.wilhite@datastax.com>
2020-11-17 10:14:51 -0500

    step-3 init

--- src/store/gameSlice.js
+++ src/store/gameSlice.js
@@ -9,9 +9,14 @@ export const slice = createSlice({
   initialState,
   reducers: {
     // Step 2: let's add a reducer that sets the game id
+    setId: (state, action) => {
+      state.id = action.payload;
+    },
   },
 });
 
 // Step 2: let's return an action and a selector
+export const { setId } = slice.actions;
+export const selectId = (state) => state.game.id;
 
 export default slice.reducer;
