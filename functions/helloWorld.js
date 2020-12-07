479897adb7d370763d530eaaa5b724ae730f5e25
David Jones-Gilardi <david.gilardi@datastax.com>
2020-12-07 19:03:34 +0000

    test2

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ hello: "test" }),
+    body: JSON.stringify({ hello: "test2" }),
   };
 };
