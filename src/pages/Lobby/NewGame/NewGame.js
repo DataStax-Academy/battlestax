218ac85bc472ba0d86ac0dac437e59d80c468ba5
Nasko Vasilev <magiccrafter@gmail.com>
2020-12-09 22:24:34 +0100

    step 4, bind redux to the User Interface

--- src/pages/Lobby/NewGame/NewGame.js
+++ src/pages/Lobby/NewGame/NewGame.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Button, Grid, Typography } from "@material-ui/core";
-//let's import what we need
+import { useDispatch, useSelector } from "react-redux";
+import { selectGame, createGame } from "../../../store/gameSlice";
 
 export default function NewGame() {
-  // let's connect Redux to our Component
+  const dispatch = useDispatch();
+  const { id, idError, idLoading } = useSelector(selectGame);
 
   return (
     <Grid container direction="row" justify="center" alignItems="center">
@@ -14,8 +16,8 @@ export default function NewGame() {
         </Typography>
         <Typography color="textSecondary">game code</Typography>
         <Typography variant="h1" className="highlight">
-          {/* let's display the game id */}
-          {"----"}
+        {/* let's display the game id */}
+        {id || "----"}
         </Typography>
         <br />
         {/* let's make our button create a new game*/}
@@ -25,10 +27,17 @@ export default function NewGame() {
           size="large"
           variant="contained"
           color="primary"
+          disabled={idLoading}
+          onClick={() => {
+            dispatch(createGame());
+          }}
         >
           start new game
         </Button>
         {/* let's show an error message if there is one */}
+        {idError && (
+          <Typography color="textSecondary">Error: {idError}</Typography>
+        )}
       </Grid>
     </Grid>
   );
