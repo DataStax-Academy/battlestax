a9fc2c5e3de73004b1503b65e82554d75d5f1960
Nasko Vasilev <magiccrafter@gmail.com>
2020-12-09 22:08:18 +0100

    step 3, client state with Redux

--- src/store/gameSlice.js
+++ src/store/gameSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { generateGameId } from "../util/random";
 
 export const initialState = {
   id: "",
@@ -10,26 +11,49 @@ export const slice = createSlice({
   name: "game",
   initialState,
   reducers: {
-    // let's add a reducer that sets the game id
-    // let's add a reducer that sets an error message
-    // let's add a reducer that sets a loading state
+    setId: (state, action) => {
+      state.id = action.payload;
+    },
+    setIdError: (state, action) => {
+      state.idError = action.payload;
+    },
+    setIdLoading: (state, action) => {
+      state.idLoading = action.payload;
+    }
   },
 });
 
 // let's export our actions and selectors
+export const { setId, setIdLoading, setIdError } = slice.actions;
+export const selectGame = (state) => state.game;
 
 // let's create an asnyc action to create a new game
 export const createGame = () => {
   return async (dispatch) => {
     // let's set the id state back to the defaults
+    dispatch(setIdLoading(true));
+    dispatch(setIdError(""));
+    dispatch(setId(""));
     try {
       // let's generate a new game id
+      const gameId = generateGameId();
       // let's call our insert game netlify function
+      const res = await fetch(`/.netlify/functions/insertGame/${gameId}`, {
+        method: "POST",
+        body: JSON.stringify({ state: "initialized" }),
+      });
+      if (!res.ok) {
+        throw Error(res.statusText);
+      }
       // let's set the game id
+      const resJson = await res.json();
+      dispatch(setId(resJson.documentId));
     } catch (e) {
       // let's set the id error if there is one
+      dispatch(setIdError(e.message));
     }
     // let's set the id state to not loading
+    dispatch(setIdLoading(false));
   };
 };
 
